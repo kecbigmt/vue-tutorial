fc583e6ab0b42f8f5f59c5a18568f580b3621668
kecy <lpfmp.ae.k@gmail.com>
2019-04-14 20:50:12 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Vue.js公式チュートリアルをやってみた
+https://jp.vuejs.org/
---
+++ js/main.js
@@ -0,0 +1,101 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+      message: 'Hello Vue!'
+    }
+  });
+  
+  var app2 = new Vue({
+    el: '#app-2',
+    data: {
+      message: 'You loaded this page on ' + new Date().toLocaleString()
+    }
+  });
+  
+  var app3 = new Vue({
+    el: '#app-3',
+    data: {
+      seen: true
+    }
+  });
+  
+  var app4 = new Vue({
+    el: '#app-4',
+    data: {
+      todos: [
+        { text: 'Learn JavaScript' },
+        { text: 'Learn Vue' },
+        { text: 'Build something awesome' }
+      ]
+    }
+  });
+  
+  var app5 = new Vue({
+    el: '#app-5',
+    data: {
+      message: 'Hello Vue.js!'
+    },
+    methods: {
+      reverseMessage: function() {
+        this.message = this.message.split('').reverse().join('');
+      }
+    }
+  });
+  
+  var app6 = new Vue({
+    el: '#app-6',
+    data: {
+      message: 'Hello Vue!'
+    }
+  });
+  
+  Vue.component('todo-item', {
+    props: ['todo'],
+    template: '<li>{{ todo.text }}</li>'
+  });
+  
+  var app7 = new Vue({
+    el: '#app-7',
+    data: {
+      groceryList: [
+        { id: 0, text: 'Vegetables' },
+        { id: 1, text: 'Cheese' },
+        { id: 2, text: 'Whatever else humans are supposed to eat'}
+      ]
+    }
+  })
+  
+  var watchExampleVM = new Vue({
+    el: '#watch-example',
+    data: {
+      question: '',
+      answer: 'I cannot give you an answer until you ask a question!',
+    },
+    watch: {
+      // この関数はquestionが変わるごとに実行されます
+      question: function(newQuestion, oldQuestion) {
+        this.answer = 'Waiting for you to stop typing...';
+        this.debouncedGetAnswer();
+      }
+    },
+    created: function() {
+      this.debouncedGetAnswer = _.debounce(this.getAnswer, 500);
+    },
+    methods: {
+      getAnswer: function() {
+        if (this.question.indexOf('?') === -1) {
+          this.answer = 'Questions usually contain a question mark. ;-)';
+          return;
+        }
+        this.answer = 'Thinking...';
+        var vm = this
+        axios.get('https://yesno.wtf/api')
+          .then(function(response) {
+          vm.answer = _.capitalize(response.data.answer)
+        })
+        .catch(function(error) {
+          vm.answer = 'Error! Could not reach the API. ' + error
+        });
+      }
+    }
+  })
